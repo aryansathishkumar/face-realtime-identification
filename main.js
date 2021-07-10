857b5558043cbcb6c5f9482263c023762cb4ff07
aryansathishkumar <76153434+aryansathishkumar@users.noreply.github.com>
2021-07-10 20:12:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function preload()
+{
+
+}
+
+function setup() {
+  canvas = createCanvas(250, 250);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  video.center()
+  classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/uhv0oQyfc/model.json",modelloaded);
+}
+
+function draw()
+{
+  image(video, 0, 0, 250 ,250);
+  classifier.classify(video, getresult);
+}
+
+function modelloaded()
+{
+  console.log(ml5.version,"model is loaded");
+}
+
+function getresult(error, result)
+{
+  if(error)
+  {
+    console.error(error);
+  }
+  else
+  {
+    console.log(result);
+    
+    document.getElementById("object2").innerHTML = result[0].label;
+    document.getElementById("accuracy2").innerHTML = result[0].confidence.toFixed(2)*100 + "%";
+  }
+}
+
+
+
+
+
